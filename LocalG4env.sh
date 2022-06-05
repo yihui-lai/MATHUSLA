42ffc2b7fa28f1d4a580658bf47c7d3fb3512ad5
Yucun-Xie <yucun.xie@cern.ch>
2022-06-05 19:42:25 -0400

    Create LocalG4env.sh

---
+++ LocalG4env.sh
@@ -0,0 +1,14 @@
+export Geant4_DIR=/Users/yucun_xie/G4.10/lib/Geant4-10.7.3/
+export G4NEUTRONHPDATA="/Users/yucun_xie/G4.10/share/Geant4-10.7.3/data/G4NDL4.6"
+export G4LEDATA="/Users/yucun_xie/G4.10/share/Geant4-10.7.3/data/G4EMLOW7.13"
+export G4LEVELGAMMADATA="/Users/yucun_xie/G4.10/share/Geant4-10.7.3/data/PhotonEvaporation5.7"
+export G4RADIOACTIVEDATA="/Users/yucun_xie/G4.10/share/Geant4-10.7.3/data/RadioactiveDecay5.6"
+export G4PARTICLEXSDATA="/Users/yucun_xie/G4.10/share/Geant4-10.7.3/data/G4PARTICLEXS3.1.1"
+export G4PIIDATA="/Users/yucun_xie/G4.10/share/Geant4-10.7.3/data/G4PII1.3"
+export G4REALSURFACEDATA="/Users/yucun_xie/G4.10/share/Geant4-10.7.3/data/RealSurface2.2"
+export G4SAIDXSDATA="/Users/yucun_xie/G4.10/share/Geant4-10.7.3/data/G4SAIDDATA2.0"
+export G4ABLADATA="/Users/yucun_xie/G4.10/share/Geant4-10.7.3/data/G4ABLA3.1"
+export G4INCLDATA="/Users/yucun_xie/G4.10/share/Geant4-10.7.3/data/G4INCL1.0"
+export G4ENSDFSTATEDATA="/Users/yucun_xie/G4.10/share/Geant4-10.7.3/data/G4ENSDFSTATE2.3"
+DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH:$PWD/lib
+
