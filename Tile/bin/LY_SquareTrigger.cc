e06e29b1e350ead26abf8296b17c7e4919c0475e
Yucun-Xie <yucun.xie@cern.ch>
2022-08-13 21:50:48 -0400

    Update baseline setup

--- Tile/bin/LY_SquareTrigger.cc
+++ Tile/bin/LY_SquareTrigger.cc
@@ -23,7 +23,7 @@ int main(int argc, char **argv)
       ("tileY,Y", usr::po::defvalue<double>(20), "Y of tile [mm]")                                    //
       ("beamwidth,w", usr::po::defvalue<double>(30), "width of beam [mm]")                            //
       ("fiberZ,f", usr::po::defvalue<float>(5), "fiber length [m]")                                   //
-      ("fiberZshift,s", usr::po::defvalue<double>(1.7), "fiber shift [m]")                            //
+      ("fiberZshift,s", usr::po::defvalue<double>(0), "fiber shift [m]")                              //
       ("absmult,a", usr::po::defvalue<double>(1000), "absorption length at 425nm, unit mm")           //
       ("tiledecay,td", usr::po::defvalue<double>(2.5), "tile decay time, unit ns")                    //
       ("tilerise,tr", usr::po::defvalue<double>(0.5), "tile rise time, unit ns")                      //
@@ -31,7 +31,7 @@ int main(int argc, char **argv)
       ("yield,y", usr::po::defvalue<double>(10), "light yield / MeV")                                 //
       ("wrapreflect,m", usr::po::defvalue<float>(0.985), "Wrap reflectivity")                         //
       ("sipmeff,e", usr::po::defvalue<double>(1), "SiPM eff")                                         //
-      ("Y11decayTime,d", usr::po::defvalue<double>(11.5), "Y11 WLS time constant")                    //
+      ("Y11decayTime,d", usr::po::defvalue<double>(7.193), "Y11 WLS time constant")                   //
       ("NEvents,N", usr::po::defvalue<unsigned>(1), "Number of events to run")                        //
       ("useProton,P", usr::po::defvalue<int>(1), "Flag to switch the source to a true proton source") //
       ("handwrap,H", usr::po::defvalue<int>(0), "Flag to switch to handwrap")                         //
